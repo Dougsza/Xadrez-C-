403abfafb5da0096fa48d6dbed04825464717f8a
Douglas de Souza Silva <douglassouzasilva888@gmail.com>
2021-04-01 14:46:55 -0300

    Criando classe Posicao

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+            p = new Posicao(5,7);
+
+            Console.WriteLine(p);
         }
     }
 }
---
+++ Xadrez-Console/Tabuleiro/Posicao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Tabuleiro {
+    class Posicao {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+       public  Posicao(int linha , int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Douglas");
+
+            return sb.ToString();
+        }
+    }
+}
