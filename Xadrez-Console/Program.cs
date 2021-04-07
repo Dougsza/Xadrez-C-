ec15ea0ff6a77b93f1ea8cd20787f54e8af0bdb7
Douglas de Souza Silva <douglassouzasilva888@gmail.com>
2021-04-07 17:29:34 -0300

    Tratando os erros com excecao personalizada

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -5,12 +5,16 @@ using xadrez;
 namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
-            Tabuleiro_Classe tab = new Tabuleiro_Classe(8,8);
-          
-            tab.ColocarPeca(new Rei(Cor.Branca,tab), new Posicao(0,1));
 
-            Tela.ImprimeTabileiro(tab);
+            try {
+                Tabuleiro_Classe tab = new Tabuleiro_Classe(8,8);
 
+                tab.ColocarPeca(new Rei(Cor.Branca,tab),new Posicao(0,1));
+                tab.ColocarPeca(new Torre(Cor.Amarela,tab),new Posicao(0,8));
+                Tela.ImprimeTabileiro(tab);
+            }catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- Xadrez-Console/Tabuleiro/Tabuleiro_Classe.cs
+++ Xadrez-Console/Tabuleiro/Tabuleiro_Classe.cs
@@ -15,12 +15,42 @@ namespace tabuleiro {
             pecas = new Peca_Tabuleiro[linhas,colunas];
         }
 
+        //Este método ajuda a verificar se dentro da matriz Peca_Tabuleiro esta vazia (null)
         public Peca_Tabuleiro peca(int linha,int coluna) {
             return pecas[linha,coluna];
-        }
+        }     
+
+        public Peca_Tabuleiro peca(Posicao pos) {
+            return pecas[pos.linha,pos.coluna];
+        }   
+
+        //Coloca a peça em uma posição do Tabuleiro
         public void ColocarPeca(Peca_Tabuleiro peca,Posicao posicao) {
+            if(ExistePeca(posicao)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
             pecas[posicao.linha,posicao.coluna] = peca;
             peca.posicao = posicao;
         }   
+
+        public bool ExistePeca(Posicao pos) {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        //Verifica se a posição é valida
+        public bool PosicaoValida(Posicao pos) {
+            if(pos.linha <0 || pos.linha >=linhas || pos.coluna <0 || pos.coluna >=colunas) {
+                return false;
+            }
+            return true;
+        }
+        /*Verifica o resultado que retornou do método PosicaoValida e caso o retorno do
+         método for falso ele envia uma Exception personalizada para a classe TabuleiroExpection*/
+        public void ValidarPosicao(Posicao pos) {
+            if(!PosicaoValida(pos)) {
+                throw new TabuleiroException("Posição invalida!");
+            }
+        }
     }    
 }
